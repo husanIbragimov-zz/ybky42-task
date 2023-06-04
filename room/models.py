67bc8c42b39df21177a4544b22672b04cd4ba4e0
husanIbragimov <xasanboyibragimov3@gmail.com>
2023-06-04 21:10:28 +0500

    create models of impact

--- room/models.py
+++ room/models.py
@@ -1,3 +1,40 @@
+from datetime import datetime
+
 from django.db import models
 
-# Create your models here.
+TYPE = (
+    ('none', 'None'),
+    ('focus', 'Focus'),
+    ('team', 'Team'),
+    ('conference', 'Conference')
+)
+
+
+class Room(models.Model):
+    name = models.CharField(max_length=223, null=True)
+    type = models.CharField(choices=TYPE, max_length=223, default='none')
+    capacity = models.IntegerField(default=0)
+    is_active = models.BooleanField(default=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Book(models.Model):
+    name = models.CharField(max_length=223, null=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+
+class FreeTime(models.Model):
+    resident = models.ForeignKey(Book, on_delete=models.SET_NULL, null=True)
+    room = models.ForeignKey(Room, on_delete=models.SET_NULL, null=True, related_name='times')
+    start = models.DateTimeField(default=datetime.now(), null=True, blank=True)
+    end = models.DateTimeField(default=datetime.now(), null=True, blank=True)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f'The {self.room} room was booked'
