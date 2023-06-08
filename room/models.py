3c4839313553c7d8fb2ec6858e6ee25d73121294
husanIbragimov <xasanboyibragimov3@gmail.com>
2023-06-09 00:01:59 +0500

    set admin panel

--- room/models.py
+++ room/models.py
@@ -19,7 +19,7 @@ class Room(models.Model):
     date_created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.id
+        return f"{self.id}"
 
 
 class User(models.Model):
