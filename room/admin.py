b58d1c72a6e9b06e3356dc96e2d6b81096ea7583
husanIbragimov <xasanboyibragimov3@gmail.com>
2023-06-08 23:57:30 +0500

    set admin panel

--- room/admin.py
+++ room/admin.py
@@ -2,7 +2,10 @@ from django.contrib import admin
 from .models import Room, User, Book
 
 
-admin.site.register(Room)
+@admin.register(Room)
+class RoomAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'type')
+
+
 admin.site.register(User)
 admin.site.register(Book)
-
