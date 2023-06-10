11a6a10b75f6d0dd817a867714d6fc9aa518a8db
husanIbragimov <xasanboyibragimov3@gmail.com>
2023-06-10 20:45:12 +0500

    read only date_created

--- room/admin.py
+++ room/admin.py
@@ -5,10 +5,12 @@ from .models import Room, User, Book
 @admin.register(Room)
 class RoomAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'type')
+    readonly_fields = ('date_created',)
 
 
 class BookAdmin(admin.ModelAdmin):
     date_hierarchy = 'start'
+    readonly_fields = ('date_created',)
 
 
 admin.site.register(User)
