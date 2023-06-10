49831546978cd01015fd6760dab46398806820e5
husanIbragimov <xasanboyibragimov3@gmail.com>
2023-06-10 19:37:31 +0500

    add cors

--- impact/settings.py
+++ impact/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = 'django-insecure-7gom_*fd4$(80*0mm^fke9^9!ry3zm5c8y4xpbl(7g9=(f($e4
 DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
-
 
 # Application definition
 
@@ -60,10 +58,9 @@ MIDDLEWARE = [
 ]
 
 # cors headers ->
-CORS_ALLOWED_ORIGINS = (
-    "http://127.0.0.1:3000",
-    "http://localhost:3000",
-)
+CORS_ALLOWED_ORIGINS = [
+    "*"
+]
 
 CORS_ALLOW_METHODS = [
     '*'
@@ -94,7 +91,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'impact.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
@@ -104,7 +100,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # ==================================================== PostgresSQL =====================================================
 # DATABASES = {
@@ -137,7 +132,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
@@ -148,7 +142,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
